491fd57f047675e64ab8d28322516d923f51d4a1
furkankinli <furkan.kinli@ozu.edu.tr>
2016-03-26 14:28:12 +0200

    Added files via upload

---
+++ project/helloworld/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from helloworld.models import Job
+
+def index(request):
+   jobs = Job.objects.all()
+   context_dict = {'job':jobs}
+   return render(request, 'helloworld/index.html', context_dict)
